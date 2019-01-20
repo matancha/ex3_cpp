015472e8dde11b73a9f38a4a28b1eaf3cfc18423
Matan Toledano <matancha@cs.huji.ac.il>
2019-01-20 16:15:16 +0200

    checkcheck

--- TimeChecker.cpp
+++ TimeChecker.cpp
@@ -18,34 +18,41 @@ int main(int argc, char *argv[])
 
     std::cout << "size " << matrixNum << std::endl;
 
-    Eigen::MatrixXd eigenMat1 = Eigen::MatrixXd::Random(matrixNum, matrixNum);
-    Eigen::MatrixXd eigenMat2 = Eigen::MatrixXd::Random(matrixNum, matrixNum);
-    Eigen::MatrixXd plusEigenMat = Eigen::MatrixXd::Random(matrixNum, matrixNum);
-    Eigen::MatrixXd multEigenMat = Eigen::MatrixXd::Random(matrixNum, matrixNum);
+  //  Eigen::MatrixXd eigenMat1 = Eigen::MatrixXd::Random(matrixNum, matrixNum);
+//    Eigen::MatrixXd eigenMat2 = Eigen::MatrixXd::Random(matrixNum, matrixNum);
+//    Eigen::MatrixXd plusEigenMat;
+ //   Eigen::MatrixXd multEigenMat;
 
-    std::vector<int> cells;
+    std::vector<int> cells(matrixNum * matrixNum);
     for (int i = 0; i < matrixNum * matrixNum; ++i)
     {
-        cells.push_back(1);
+        cells[i] = 1;
     }
 
+    std::vector<int> cellsPlus(matrixNum * matrixNum);
+    std::vector<int> cellsMult(matrixNum * matrixNum);
+
+    std::cout << "hi" << std::endl;
+
     Matrix<int> libMat(matrixNum, matrixNum, cells);
-    Matrix<int> plusMat(matrixNum, matrixNum);
-    Matrix<int> multMat(matrixNum, matrixNum);
+    std::cout << "hi2" << std::endl;
+    Matrix<int> plusMat(matrixNum, matrixNum, cellsPlus);
+    std::cout << "hi3" << std::endl;
+    Matrix<int> multMat(matrixNum, matrixNum, cellsMult);
 
     std::stack<std::chrono::time_point<std::chrono::system_clock>> tictoc_stack;
     std::chrono::duration<double> elapsed_seconds;
 
     // Mult eigen Matrix
     tictoc_stack.push(std::chrono::system_clock::now());
-    multEigenMat = eigenMat1 * eigenMat2;
+ //   multEigenMat = eigenMat1 * eigenMat2;
     elapsed_seconds = std::chrono::system_clock::now() - tictoc_stack.top();
     std::cout << "eigen mult " << elapsed_seconds.count() << std::endl;
     tictoc_stack.pop();
 
     // Plus eigen Matrix
     tictoc_stack.push(std::chrono::system_clock::now());
-    plusEigenMat = eigenMat1 + eigenMat2;
+  //  plusEigenMat = eigenMat1 + eigenMat2;
     elapsed_seconds = std::chrono::system_clock::now() - tictoc_stack.top();
     std::cout << "eigen add " << elapsed_seconds.count() << std::endl;
     tictoc_stack.pop();
